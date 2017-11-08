2b79dbc52599da91f445e884088025f50a56eeac
Joe Wass <jwass@crossref.org>
2017-11-08 19:46:41 +0000

    Increase partitions for more potential parallelism.

--- create-topics.sh
+++ create-topics.sh
@@ -1,7 +1,7 @@
-docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_sample --partitions 10 --replication-factor 1 --if-not-exists'
+docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_sample --partitions 100 --replication-factor 1 --if-not-exists'
 
-docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_observation --partitions 10 --replication-factor 1 --if-not-exists'
+docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_observation --partitions 100 --replication-factor 1 --if-not-exists'
 
-docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_extraction --partitions 10 --replication-factor 1 --if-not-exists'
+docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_extraction --partitions 100 --replication-factor 1 --if-not-exists'
 
-docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_analysis --partitions 10 --replication-factor 1 --if-not-exists'
+docker-compose exec  kafka bash -c '$KAFKA_HOME/bin/kafka-topics.sh --zookeeper localhost --create --topic visitor_analysis --partitions 100 --replication-factor 1 --if-not-exists'
